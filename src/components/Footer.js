d2bdb604f569166380a69c8a76ce57725fb5f55a
MVCorreas <correasmv@gmail.com>
2025-08-28 18:25:15 -0300

    feat: added Order component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,15 @@
+import Order from "./Order";
+
 export default function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-  // else alert("Sorry we're closed");
-
-  // if (!isOpen) return <p>CLOSED</p>;
 
   return (
     <footer className="footer">
       {isOpen ? (
-        <p> We are open!</p>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
