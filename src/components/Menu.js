d099eb320a5a9f226804136735b8562ab185bdc8
MVCorreas <correasmv@gmail.com>
2025-08-28 18:18:26 -0300

    feat: added pizzas to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,36 @@
+import pizzaData from "../utils/pizzaData";
+
 export default function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <p>Delicious pizzas made with fresh ingredients!</p>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <li
+                key={pizza.name}
+                className={`pizza ${pizza.soldOut ? "sold-out" : ""}`}
+              >
+                <img src={pizza.photoName} alt={pizza.name} />
+                <h3>{pizza.name}</h3>
+                <p>{pizza.ingredients}</p>
+                <p className="price">${pizza.price.toFixed(2)}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
     </main>
   );
 }
